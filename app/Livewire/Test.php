63672658e14aca9655151c187a1d1b6896f42064
Lucas <lucazmoura2110@gmail.com>
2024-10-08 00:25:31 -0300

    Livewire commit

---
+++ app/Livewire/Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Test extends Component
+{
+
+    public string $search = '';
+
+    public function render()
+    
+    {
+        return view('livewire.test', [
+
+        'users' => User::query()
+
+        ->when($this->search, fn($q) => $q->where('name', 'like', "%{$this->search}%"))
+        ->get()
+
+        ]);
+    }
+}
