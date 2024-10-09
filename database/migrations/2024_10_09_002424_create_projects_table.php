f811433a28ffc499e319b060239767d8d5a187fe
Lucas <lucazmoura2110@gmail.com>
2024-10-08 21:42:01 -0300

    Debbugin users

--- database/migrations/2024_10_09_002424_create_projects_table.php
+++ database/migrations/2024_10_09_002424_create_projects_table.php
@@ -7,12 +7,21 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+      tittle string
+  description text
+  created_at datetime
+  ends_at datetime
+  status string
+  tech_stack json
+  created_by fk [ref: < users.id]
      */
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $
+
             $table->timestamps();
         });
     }
