e59ce0d4a68e19bc85a3afc5d92b60862825a76b
Lucas <lucazmoura2110@gmail.com>
2024-10-09 22:18:26 -0300

    ProposalFactory

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app>
+    <a href="{{ route('projects.show', 1)}}">
+
+    primeiro projeto
+
+    </a>
+</x-layouts.app>          
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app>
+   show
+</x-layouts.app>     
